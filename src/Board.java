4250a5d59ef3993aff85df8155562a5db8f3442a
suhamansoor <suha.mansoor@emu.edu>
2014-09-08 13:36:58 -0400

    made a simple board

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,31 @@
 
 public class Board {
-	public Board () {
-		final char[][] board;
-		
-		
-	}
-	
+	static char[][] board;
+	public static void main(String args[]){
+		board = new char[6][7];
+		fillBoard();
 
+	}
+	private static void fillBoard(){
+		char blank = '#';
+		for (int row=0; row<6; row++){
+			for(int col=0; col<7; col++){
+				board [row][col]= blank;	
+			}	
+		}
+		printBoard(board);
+	}
+
+	private static void printBoard (char [][] brd){
+
+		for (int row=0; row<6; row++){
+			for(int col=0; col<7; col++){
+				System.out.print (brd[row][col]);
+
+			}
+			System.out.println ();
+		}
+	}
 }
+
+
